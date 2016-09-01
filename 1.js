c2fa3e67b575685149454ff93513efc760746a6f
nizp <nzp890524@163.com>
2016-09-01 15:50:36 +0800

    js and index

--- 1.js
+++ 1.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded',fn);
 function fn(){
+	function Tab(){
+		this.div = document.getElementsByTagName('div');
+		this.btn = document.getElementsByTagName('input');
+	}
+	
+	Tab.prototype.clicks = function(){
+		for(var i=0;i<this.btn.length;i++){
+			this.btn[i].addEventListener('click',function(){
+								
+			});
+		}
+	}
 	
 }
